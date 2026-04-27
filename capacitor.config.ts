314e6f5c26f495241bfae8538004ebd0abd39dc6
ABDOUL-MOUHSIN-CHOUAIB-CHERIF <abdoulmouhsincherif@gmail.com>
2026-04-27 10:50:00 +0100

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EXPENSE_TRACKER',
+  webDir: 'www'
+};
+
+export default config;
